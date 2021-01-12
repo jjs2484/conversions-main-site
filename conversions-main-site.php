f73173d83f04827beeeebdd801adb7843daa7e63
Jesse S <3428918+jjs2484@users.noreply.github.com>
2021-01-11 22:58:39 -0500

    bump version

--- conversions-main-site.php
+++ conversions-main-site.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Conversions Main Site
  * Description: Styles, scripts, and functions.
- * Version: 1.0.3
+ * Version: 1.0.4
  * Author: js2484
  * Author URI: https://conversionswp.com
  */
