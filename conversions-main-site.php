aadc6dabd5062023b37f7b1a76ea3d8270745872
Jesse S <3428918+jjs2484@users.noreply.github.com>
2021-09-30 08:43:07 -0400

    BS5 kitchen sink changes

--- conversions-main-site.php
+++ conversions-main-site.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Conversions Main Site
  * Description: Styles, scripts, and functions.
- * Version: 1.0.7
+ * Version: 1.0.8
  * Author: js2484
  * Author URI: https://conversionswp.com
  */
